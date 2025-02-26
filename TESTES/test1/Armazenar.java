4151990a4e4fbc30565d44427ca4a90fa1d2a477
Aline Soares <aliinanda@example.com>
2025-02-26 17:14:13 -0300

    alal

--- TESTES/test1/Armazenar.java
+++ TESTES/test1/Armazenar.java
@@ -1,3 +1,4 @@
+package test1;
 import java.util.ArrayList;
 
 public class Armazenar {
--- TESTES/test1/Main.java
+++ TESTES/test1/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import test1.Armazenar;
 
 public class Main{
     public static void main(String args[]){
