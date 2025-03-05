c0c31353fe177c0b4dce545557033d86fdb27aca
Aline Soares <aliinanda@example.com>
2025-03-05 20:57:22 -0300

    AA

--- TESTES/test5/Gerenciador.java
+++ TESTES/test5/Gerenciador.java
@@ -24,11 +24,46 @@ public class Gerenciador {
         encontrou = false;
         for( Livro l  :  array){
             if(l.status.equals(status)){
-                System.out.println("titulo: ");
+                System.out.println("----------------------");
+                System.out.println("titulo: " + l.titulo);
+                System.out.println("autor: " + l.autor);
+                System.out.println("numero de paginas: " + l.numeroPaginas);
+                System.out.println("status de leitura: " + l.status);
+                System.out.println("----------------------");
+                encontrou = true;
+            }
+            if(!encontrou){
+                System.out.println("Livro não encontrado");
             }
         }
     }
 
+    public void listar(){
+        if(array.isEmpty()){
+            System.out.println("Lista de livros vazia");
+        }
+        for(Livro l : array){
+            System.out.println("----------------------");
+            System.out.println("Lista de Livros:");
+            System.out.println("----------------------");
+            System.out.println("titulo: " + l.titulo);
+            System.out.println("autor: " + l.autor);
+            System.out.println("numero de paginas: " + l.numeroPaginas);
+            System.out.println("status de leitura: " + l.status);
+            System.out.println("----------------------");
+        }
+    }
 
+    public void atualizar(String status, string titulo){
+        Livro l = array.get(titulo);
+        l.status = status;
+        array.set(index, l);
+        System.out.println("Status foi atualizado!");
+    }
+
+    public void remover(int titulo){
+        array.remove(titulo);
+        System.out.println("O livro foi removido!");
+    }
     
 }
